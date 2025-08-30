bc1cd53913750fd310998f387d6855497849f18b
sillyburg <sillyburgerprivalt@gmail.com>
2025-08-30 12:20:55 -0300

    I FORGOT THE ANGLE SHIT

--- scripts/Camera_Movement.lua
+++ scripts/Camera_Movement.lua
@@ -1,7 +1,10 @@
 local intensity = 25
+local angleIntensity = 1
+
 local offset = {
     x = 0,
-    y = 0
+    y = 0,
+    angle = 0
 }
 
 function goodNoteHit(index, noteData, noteType, isSustain)
@@ -13,15 +16,16 @@ function opponentNoteHit(index, noteData, noteType, isSustain)
 end
 
 function moveCam(index)
-    if index == 0 then setOffsetPos({-intensity, 0}) end
-    if index == 1 then setOffsetPos({0, intensity}) end
-    if index == 2 then setOffsetPos({0, -intensity}) end
-    if index == 3 then setOffsetPos({intensity, 0}) end
+    if index == 0 then setOffsetPos({-intensity, 0, -angleIntensity}) end
+    if index == 1 then setOffsetPos({0, intensity, 0}) end
+    if index == 2 then setOffsetPos({0, -intensity, 0}) end
+    if index == 3 then setOffsetPos({intensity, 0, angleIntensity}) end
 end
 
 function setOffsetPos(posArray)
     offset.x = posArray[1]
     offset.y = posArray[2]
+    offset.angle = posArray[3]
 end
 
 function onUpdate(elapsed)
@@ -29,8 +33,9 @@ function onUpdate(elapsed)
 
     setProperty('camGame.targetOffset.x', lerp(getProperty('camGame.targetOffset.x'), offset.x, elapsed * 2.4 * getProperty('cameraSpeed') * playbackRate))
     setProperty('camGame.targetOffset.y', lerp(getProperty('camGame.targetOffset.y'), offset.y, elapsed * 2.4 * getProperty('cameraSpeed') * playbackRate))
+    setProperty('camGame.angle', lerp(getProperty('camGame.angle'), offset.angle, elapsed * 2.4 * getProperty('cameraSpeed') * playbackRate))
 
-    if not stringStartsWith(getProperty((mustHitSection and "boyfriend" or "dad")..'.animation.curAnim.name'), "sing") then setOffsetPos({0, 0}) end
+    if not stringStartsWith(getProperty((mustHitSection and "boyfriend" or "dad")..'.animation.curAnim.name'), "sing") then setOffsetPos({0, 0, 0}) end
 end
 
 function lerp(a, b, t) return a + (b - a) * t end
